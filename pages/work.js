f8db1bf5dbbfae068e11f0c68615c970c0aa0e63
Florian Schneider <schneider.f@me.com>
2022-11-04 00:45:42 +0100

    link fixed

--- pages/work.js
+++ pages/work.js
@@ -47,9 +47,22 @@ export default function Home() {
         <a>
           Florian Schneider Creative Developer
         </a>
-        <a>
-          LinkedIn, Github
-        </a>
+
+        <ul className={styles.commalist}>
+          <li>
+            <Link href="https://www.linkedin.com/in/florianschneiderberlin/" legacyBehavior>
+              <a target="_blank" className={styles.link}>LinkedIn</a>
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.commalist}>
+          <li>
+            <Link href="https://github.com/coodaa" legacyBehavior>
+              <a target="_blank" className={styles.link}>Github</a>
+            </Link>
+          </li>
+        </ul>
+
         <a>
           Design & Development Florian Schneider
         </a>
