ff66b5720392f4a57f1fb55d14f5a2879f9b57ec
Florian Schneider <schneider.f@me.com>
2022-11-19 18:56:14 +0100

    minor changes about style

--- components/Footer.js
+++ components/Footer.js
@@ -5,40 +5,41 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.row}>
-      <ul className={styles.commalist}>
-        <li className={styles.myself}>
-          <a>Florian Schneider</a>
-          <a>Creative Developer</a>
-        </li>
-      </ul>
-      <ul className={styles.commalist}>
+        <ul className={styles.commalist}>
+          <li className={styles.myself}>
+            <a>Florian Schneider</a>
+            <a>Creative Developer</a>
+          </li>
+        </ul>
+        <ul className={styles.commalist}>
+          <li>
+            <Link href="https://www.linkedin.com/in/florianschneiderberlin/" legacyBehavior>
+              <a target="_blank" className={styles.link}>LinkedIn</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/coodaa" legacyBehavior>
+              <a target="_blank" className={styles.link}>Github</a>
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.commalist}>
+          <li className={styles.myself}>
+            <a>Design & Development</a>
+            <a>Florian Schneider</a>
+          </li>
+        </ul>
+      </div>
+      <ul className={styles.imprint}>
         <li>
-          <Link href="https://www.linkedin.com/in/florianschneiderberlin/" legacyBehavior>
-            <a target="_blank" className={styles.link}>LinkedIn</a>
-          </Link>
+          <a>2022 Florian Schneider</a>
         </li>
         <li>
-          <Link href="https://github.com/coodaa" legacyBehavior>
-            <a target="_blank" className={styles.link}>Github</a>
-          </Link>
-        </li>
-      </ul>
-      <ul className={styles.commalist}>
-        <li className={styles.myself}>
-          <a>Design & Development</a>
-          <a>Florian Schneider</a>
-        </li>
-      </ul>
-    </div>
-
-      <ul className={styles.commalist}>
-        <li className={styles.imprint}>
           <Link href="/imprint" legacyBehavior>
             <a className={styles.link}>Imprint</a>
           </Link>
         </li>
       </ul>
-
     </footer>
   );
 }
