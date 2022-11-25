7663fc42606ab2225b865efe381adbd8fb3e8c55
Florian Schneider <schneider.f@me.com>
2022-11-25 15:13:54 +0100

    cleaned up

--- pages/work.js
+++ pages/work.js
@@ -1,21 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Work.module.css'
 import React, { useEffect } from 'react';
-import { gsap } from 'gsap'
 import SplitType from 'split-type'
-
+import { gsap } from "gsap/dist/gsap";
+import '../styles/Work.module.css'
 
 export default function Home() {
 
-
-  // const header = React.createRef();
-  // useEffect(() => {
-  //   gsap.to(header.current, {color: "#8c0", duration: 5})
-  // }, [header]);
-
+  const header = React.createRef();
+  useEffect(() => {
+    gsap.to(header.current, {color: "#8c0", duration: 5})
+  }, [header]);
 
   useEffect(() => {
-    const title = document.querySelector("#myText");
+    const title = document.querySelector('#myText');
     const myText = new SplitType(title)
     gsap.to(".char", {
       y: 0,
@@ -26,21 +24,6 @@ export default function Home() {
     }, [myText]);
   })
 
-
-
-  // if(typeof window !== "undefined"){
-  //     const myText = new SplitType('#myText')
-
-  //   useEffect(() => {
-  //     gsap.to('.char', {
-  //       y: 0,
-  //       stagger: 0.05,
-  //       delay: 0.2,
-  //       duration: .1
-  //     })
-  //   }, [myText]);
-  // }
-
   return (
     <div>
       <Head>
@@ -49,9 +32,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <main className={styles.work}>
-          {/* <h1 ref={header}>Creative Developer</h1> */}
+          <h1 ref={header}>Creative Developer</h1>
 
-          <h1 id="myText" className={styles.myText}>Creative Developer</h1>
+          <h1 id="myText">Creative Developer</h1>
 
         </main>
     </div>
