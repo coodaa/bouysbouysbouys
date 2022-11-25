58518437e0b99d8104a32636ad1d337582abb864
Florian Schneider <schneider.f@me.com>
2022-11-25 01:31:51 +0100

    added animation

--- pages/work.js
+++ pages/work.js
@@ -1,7 +1,46 @@
 import Head from 'next/head'
 import styles from '../styles/Work.module.css'
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap'
+import SplitType from 'split-type'
+
 
 export default function Home() {
+
+
+  // const header = React.createRef();
+  // useEffect(() => {
+  //   gsap.to(header.current, {color: "#8c0", duration: 5})
+  // }, [header]);
+
+
+  useEffect(() => {
+    const title = document.querySelector("#myText");
+    const myText = new SplitType(title)
+    gsap.to(".char", {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      stagger: 0.05,
+      delay: 0.2,
+    }, [myText]);
+  })
+
+
+
+  // if(typeof window !== "undefined"){
+  //     const myText = new SplitType('#myText')
+
+  //   useEffect(() => {
+  //     gsap.to('.char', {
+  //       y: 0,
+  //       stagger: 0.05,
+  //       delay: 0.2,
+  //       duration: .1
+  //     })
+  //   }, [myText]);
+  // }
+
   return (
     <div>
       <Head>
@@ -10,7 +49,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <main className={styles.work}>
-          <h2>WORK</h2>
+          {/* <h1 ref={header}>Creative Developer</h1> */}
+
+          <h1 id="myText" className={styles.myText}>Creative Developer</h1>
+
         </main>
     </div>
   )
