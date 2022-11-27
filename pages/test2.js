83296664fda40c17e48f487cd3c7c13c51aa6dc9
Florian Schneider <schneider.f@me.com>
2022-11-28 00:15:02 +0100

    added animation developer

--- pages/test2.js
+++ pages/test2.js
@@ -1,32 +1,40 @@
 import React, { useEffect, useRef } from "react";
 import styles from "../styles/test2.module.css";
-import styled from "@emotion/styled";
 import gsap from "gsap";
 import SplitTextToChars from "../components/SplitTextToChars.js";
 
-const TextStyled = styled.p`
-  transform: translateY(130px);
-  transition-property: transform;
-  transition-duration: 0.3;
-  display: inline-block;
-`;
-
 export default function Home() {
-  const wavyTextRef = useRef(null);
+  const upTextRef = useRef(null);
 
   useEffect(() => {
-    if (!wavyTextRef.current) return;
-    const chars = SplitTextToChars(wavyTextRef.current);
-
-    gsap.set(wavyTextRef.current, { perspective: 400 });
-
+    if (!upTextRef.current) return;
+    const chars = SplitTextToChars(upTextRef.current);
+    gsap.set(upTextRef.current, { perspective: 200 });
     gsap.from(
       chars,
       {
-        duration: 0.8,
-        delay: 0.1,
-        y: +100,
-        stagger: 0.05,
+        duration: 0.4,
+        delay: 0.0,
+        y: +90,
+        stagger: 0.03,
+      },
+      "+=0"
+    );
+  }, []);
+
+  const upTextRefi = useRef(null);
+
+  useEffect(() => {
+    if (!upTextRefi.current) return;
+    const chars = SplitTextToChars(upTextRefi.current);
+    gsap.set(upTextRefi.current, { perspective: 200 });
+    gsap.from(
+      chars,
+      {
+        duration: 0.4,
+        delay: 0.00,
+        y: +90,
+        stagger: 0.03,
       },
       "+=0"
     );
@@ -34,8 +42,13 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles["split"]}>
-        <TextStyled ref={wavyTextRef}>creative &nbsp;developer</TextStyled>
+      <div className={styles.char}>
+        <p className={styles["down-up-text"]} ref={upTextRef}>
+          creative
+        </p>
+        <p className={styles["down-up-text"]} ref={upTextRefi}>
+          developer
+        </p>
       </div>
     </main>
   );
