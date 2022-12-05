0c9613eb69c950d60fefecb0910d20c6b744e9eb
Florian Schneider <schneider.f@me.com>
2022-12-06 00:33:28 +0100

    blob

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,36 @@
-import styles from '../styles/Home.module.scss';
-import Link from 'next/link';
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
     <nav className={styles.nav}>
-        <ul className={styles.commalist}>
-          <li>
-            <Link className={styles.link} href="/">Florian Schneider</Link>
-          </li>
-        </ul>
-        <ul className={styles.commalist}>
-          <li>
-            <Link href="/work" legacyBehavior>
-              <a className={styles.link}>Work</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" legacyBehavior>
-              <a className={styles.link}>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="mailto:schneider.f@me.com" legacyBehavior>
+      <ul className={styles.commalist}>
+        <li>
+          <Link className={styles.link} href="/">
+            Florian Schneider
+          </Link>
+        </li>
+      </ul>
+      <ul className={styles.commalist}>
+        <li>
+          <Link href="/work" legacyBehavior>
+            <a className={styles.link}>Work</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" legacyBehavior>
+            <a className={styles.link}>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" legacyBehavior>
+            <a className={styles.link}>Contact</a>
+          </Link>
+          {/* <Link href="mailto:schneider.f@me.com" legacyBehavior>
               <a className={styles.link}>Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+            </Link> */}
+        </li>
+      </ul>
+    </nav>
   );
 }
-
-export default Navbar;
