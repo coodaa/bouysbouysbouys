675c6ca216eb750b76483624c7c995873c4d3ff6
Florian Schneider <schneider.f@me.com>
2023-06-02 21:55:49 +0200

    back to january

---
+++ pages/index2.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Video from "../components/Video";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>My hhh</title>
+    </Head>
+    <Video />
+  </>
+);
+
+export default Home;
