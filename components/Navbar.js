6385d5ca56af477d553ddf5cefcfb034d40ba0d5
Florian Schneider <schneider.f@me.com>
2023-06-03 10:27:03 +0200

    update mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,78 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Navbar.module.css";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isOpen]);
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const checkActive = (path) => {
+    return router.pathname === path ? styles.active : "";
+  };
+
   return (
     <nav className={styles.nav}>
-      <ul className={styles.commalist}>
-        <li>
-          <Link href="/" legacyBehavior>
-            <a className={styles.link}>Florian Schneider</a>
-          </Link>
-        </li>
-      </ul>
+      <div className={styles.leftSide}>
+        <Link href="/" legacyBehavior>
+          <a className={styles.link} onClick={closeMenu}>
+            Florian Schneider
+          </a>
+        </Link>
+      </div>
       <ul className={isOpen ? styles.commalistOpen : styles.commalist}>
+        <div className={styles.hideOnDesktop}>
+          <li>
+            <Link href="/" legacyBehavior>
+              <a
+                className={`${styles.link} ${checkActive("/")}`}
+                onClick={closeMenu}
+              >
+                Home
+              </a>
+            </Link>
+          </li>
+        </div>
         <li>
           <Link href="/work" legacyBehavior>
-            <a className={styles.link}>Work</a>
+            <a
+              className={`${styles.link} ${checkActive("/work")}`}
+              onClick={closeMenu}
+            >
+              Work
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/about" legacyBehavior>
-            <a className={styles.link}>About</a>
+            <a
+              className={`${styles.link} ${checkActive("/about")}`}
+              onClick={closeMenu}
+            >
+              About
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/contact" legacyBehavior>
-            <a className={styles.link}>Contact</a>
+            <a
+              className={`${styles.link} ${checkActive("/contact")}`}
+              onClick={closeMenu}
+            >
+              Contact
+            </a>
           </Link>
         </li>
       </ul>
